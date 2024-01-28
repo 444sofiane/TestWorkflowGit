edf1f405ebf843d2c4966a9f6d9b296369be2a06
444sofiane <sofiane.saou@epitech.eu>
2024-01-28 22:24:24 +0100

    add unit tests run

---
+++ src/my_program.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** GitAction
+** File description:
+** add
+*/
+
+// my_program.c
+#include <stdio.h>
+
+int my_add(int a, int b) {
+    return a + b;
+}
---
+++ tests/tests.c
@@ -0,0 +1,35 @@
+// test_my_program.c
+#include "../lib/criterion/include/criterion/criterion.h"
+#include "../src/my_program.c"
+
+//generate a test for my_add function
+
+// Test when both inputs are negative
+Test(my_add, should_return_negative_4_when_both_inputs_are_negative_2) {
+    int result = my_add(-2, -2);
+    cr_assert_eq(result, -4);
+}
+
+// Test when one input is negative and the other is positive
+Test(my_add, should_return_0_when_inputs_are_2_and_negative_2) {
+    int result = my_add(2, -2);
+    cr_assert_eq(result, 0);
+}
+
+// Test when one input is 0 and the other is positive
+Test(my_add, should_return_2_when_inputs_are_2_and_0) {
+    int result = my_add(2, 0);
+    cr_assert_eq(result, 2);
+}
+
+// Test when one input is 0 and the other is negative
+Test(my_add, should_return_negative_2_when_inputs_are_negative_2_and_0) {
+    int result = my_add(-2, 0);
+    cr_assert_eq(result, -2);
+}
+
+// Test when both inputs are 0
+Test(my_add, should_return_0_when_both_inputs_are_0) {
+    int result = my_add(0, 0);
+    cr_assert_eq(result, 0);
+}
